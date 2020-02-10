a2969d4c9a690d89d01b5fd4e76895f528811ea3
marianaabarcazama <marianaabarcazama@gmail.com>
2020-02-10 13:57:08 -0500

    Listed objectives

---
+++ SetCharacterization.R
@@ -0,0 +1,5 @@
+# Determine which datasets contain high enough temperatures to see a decline in survival / increase in development time
+# This will help us determine what sorts of curves we should be fitting later on
+# This could be mechanistically achieved by seeing if survival is highest (or development time is lowest) at the last temperature measured for each set
+# In those datasets that do include high temperatures, identify whether survival rate starts dropping before development time starts increasing
+# We're not sure development time will ever start increasing - the organisms might just die 
