0c24fa7c2030165a1158c874c67c8282b0ac5ecf
marianaabarcazama <marianaabarcazama@gmail.com>
2020-02-13 13:07:54 -0500

    Summary stats and graphs

--- ExploratoryFigures.R
+++ ExploratoryFigures.R
@@ -1,8 +1,3 @@
 # Create graphs showing survival and development time vs. temperature for all of the data
 # Faceted/subsetted by family and/or lifestage
 # Each set would be an individual curve 
-# Determine which datasets contain high enough temperatures to see a decline in survival / increase in development time
-# This will help us determine what sorts of curves we should be fitting later on
-# This could be mechanistically achieved by seeing if survival is highest (or development time is lowest) at the last temperature measured for each set
-# In those datasets that do include high temperatures, identify whether survival rate starts dropping before development time starts increasing
-# We're not sure development time will ever start increasing - the organisms might just die 
--- SummaryStats.R
+++ SummaryStats.R
@@ -11,13 +11,75 @@
 length(unique(Ana_rates$family)) # 23
  
 # Number of sets within each family
-# Number of sets measuring each lifestage* 
+family_sets <-  Ana_rates %>%
+  select(set, family) %>% 
+  distinct() %>% 
+  group_by(family) %>% 
+  summarise(sets = length(unique(set))) %>% 
+  arrange(sets)
 
+ggplot(family_sets, aes(x = reorder(family, - sets), y = sets)) +
+  geom_col()+
+  geom_hline(yintercept = 50)+
+  geom_hline(yintercept = 25, linetype = 2)+
+  theme_cowplot()+
+  xlab("Family")+
+  coord_flip()
+# Number of species per family
+species <-  Ana_rates %>%
+  select(family, sp) %>% 
+  distinct() %>% 
+  group_by(family) %>% 
+  summarise(sp_count = length(unique(sp))) %>% 
+  arrange(sp_count)
+
+ggplot(species, aes(x = reorder(family, - sp_count), y = sp_count)) +
+  geom_col()+
+  geom_hline(yintercept = 10, linetype = 2)+
+  geom_hline(yintercept = 5, linetype = 1)+
+  theme_cowplot()+
+  xlab("Family")+
+  ylab("Species")+
+  coord_flip()
+
+# Number of sets measuring each lifestage
+lifestage_sets <-  Ana_rates %>%
+  select(set, lifestage) %>% 
+  distinct() %>% 
+  group_by(lifestage) %>% 
+  summarise(sets = length(unique(set))) %>% 
+  arrange(sets)
+
+ggplot(lifestage_sets, aes(x = reorder(lifestage,  sets), y = sets)) +
+  geom_col()+
+  geom_hline(yintercept = 50)+
+  geom_hline(yintercept = 25, linetype = 2)+
+  theme_cowplot()+
+  xlab("Family")+
+ 
+  coord_flip()
+
+lifestage_sets_2 <-  Ana_rates %>%
+  select(set, lifestage) %>% 
+  distinct() %>% 
+  group_by(lifestage) %>% 
+  summarise(sets = length(unique(set))) %>% 
+  arrange(sets) %>% 
+  filter(sets > 4)
+ggplot(lifestage_sets_2, aes(x = reorder(lifestage,  sets), y = sets)) +
+  geom_col()+
+  geom_hline(yintercept = 50)+
+  geom_hline(yintercept = 25, linetype = 2)+
+  theme_cowplot()+
+  xlab("Family")+
+  
+  coord_flip()
 # Potentially use this as the dataset for some of our initial analyses 
 
 # 2. Data with both survival and development rate data -----------------------------------------------------
 interval_sets <- Ana_rates %>% 
   group_by(set) %>% 
+   
   mutate(dr_sum = sum(dr), 
          n_temps = length(unique(temp)), 
          validcount = sum(!is.na(dt)), 
