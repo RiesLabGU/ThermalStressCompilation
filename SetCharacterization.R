f46818d76fdf9366afcfc5dcc9c826b614155092
marianaabarcazama <marianaabarcazama@gmail.com>
2020-02-13 16:43:48 -0500

    started set characterization. work on function

--- SetCharacterization.R
+++ SetCharacterization.R
@@ -3,3 +3,29 @@
 # This could be mechanistically achieved by seeing if survival is highest (or development time is lowest) at the last temperature measured for each set
 # In those datasets that do include high temperatures, identify whether survival rate starts dropping before development time starts increasing
 # We're not sure development time will ever start increasing - the organisms might just die 
+
+# Functions
+t <- set_6$temp
+p <- set_6$dt
+is.rise <- function(t, p){
+  table <- tibble(temperature = t,
+                  performance = p)
+  #table <- filter(table, !is.na(performance))
+  p.large <- max(table$performance)
+  t.large <- filter(table, performance == p.large)[["temp"]]
+  colds <- filter(table, temperature < t.large)
+  hots <- filter(table, temperature > t.large)
+  output <- tibble(treatments = nrow(table), 
+                   colds = nrow(colds), 
+                   hots = nrow(hots))
+  output
+  
+}
+
+is.rise(set_6$temp, set_6$dt)
+
+## Nest data 
+nested <- interval_sets %>% 
+  group_by(set) %>% 
+  nest() %>% 
+  mutate(ntemps = nrow())
